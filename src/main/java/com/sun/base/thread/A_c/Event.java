53ae79bbc36a8188118a7f51fab661bae6835b91
sunyamengbuhtig <sunyameng_anis@sina.com>
2014-10-31 02:00:48 +0800

    a_2

---
+++ src/main/java/com/sun/base/thread/A_c/Event.java
@@ -0,0 +1,27 @@
+package com.sun.base.thread.A_c;
+
+import java.util.Date;
+
+/**
+ * Created by louis on 2014/10/31.
+ */
+public class Event {
+    private String event;
+    private Date date;
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
